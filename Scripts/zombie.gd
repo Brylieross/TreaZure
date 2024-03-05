80d072104a63e22eeb6042f7a7bc7e8ba5d8f15f
BURNSIDE HIGH SCHOOL <brylieross21@gmail.com>
2024-03-05 13:07:01 +1300

    Commit 7
    Added zombies with pathfinding

--- Scripts/zombie.gd
+++ Scripts/zombie.gd
@@ -4,14 +4,23 @@ extends CharacterBody3D
 
 var SPEED = 3.0
 
+
 func _physics_process(delta):
 	var current_location = global_transform.origin
-	var next_location = nav_agent.get_next_location()
+	var next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
-	velocity = new_velocity
+	nav_agent.set_velocity(new_velocity)
+
+
+func update_target_position(target_location):
+	nav_agent.set_target_position(target_location)
+
+
+func _on_navigation_agent_3d_target_reached():
+	print("in range")
+
+
+func _on_navigation_agent_3d_velocity_computed(safe_velocity):
+	velocity = velocity.move_toward(safe_velocity, .25)
 	move_and_slide()
-
-
-func update_target_lcoation(target_location):
-	nav_agent.set_target_location(target_location)
